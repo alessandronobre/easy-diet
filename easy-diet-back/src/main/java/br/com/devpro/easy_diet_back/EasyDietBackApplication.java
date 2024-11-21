afc57b5f0bde4fbcbfef6f25fe057bce441c464e
Alessandro <alessandronobre.ti@gmail.com>
2024-11-20 21:48:56 -0300

    Subindo back end do projeto

---
+++ easy-diet-back/src/main/java/br/com/devpro/easy_diet_back/EasyDietBackApplication.java
@@ -0,0 +1,13 @@
+package br.com.devpro.easy_diet_back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EasyDietBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EasyDietBackApplication.class, args);
+	}
+
+}
---
+++ easy-diet-back/src/test/java/br/com/devpro/easy_diet_back/EasyDietBackApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.devpro.easy_diet_back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EasyDietBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
